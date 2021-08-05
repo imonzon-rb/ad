c1a42452d1568940cd58c8376da17c41b371b9b6
sbx_user1051 <sbx_user1051@169.254.178.105>
2021-08-05 21:10:39 +0000

    Initial commit

---
+++ src/main/scala/com/evolution/bootcamp/assignment/poker/Solver.scala
@@ -0,0 +1,16 @@
+package com.evolution.bootcamp.assignment.poker
+
+object Solver {
+  // TODO: implement correct solution logic
+  def process(line: String): String = {
+    val ErrorPrefix = "Error: "
+
+    line.split("\\s+").toList match {
+      case "texas-holdem" :: board :: hands   => hands.sorted.mkString(" ")
+      case "omaha-holdem" :: board :: hands   => ErrorPrefix + "The solution doesn't support Omaha Hold'em"
+      case "five-card-draw" :: hands          => ErrorPrefix + "The solution doesn't support Five Card Draw"
+      case x :: _                             => ErrorPrefix + "Unrecognized game type"
+      case _                                  => ErrorPrefix + "Invalid input"
+    }
+  }
+}
